9a8e98945f6502c5afc39291f0f8b76b6a10daed
Arthur Lockman <hello@rthr.me>
2015-02-25 11:42:08 -0500

    Time and temp working, just need to add the edit mode.

--- stringutils.h
+++ stringutils.h
@@ -1,9 +1,68 @@
-#include <string.h>
 #include <time.h>
 
-inline int convert_temp(char* dest, float temp)
+inline void normalize_time(struct tm * time_ptr)
 {
-	return 0;
+    if (time_ptr->tm_sec > 60)
+    {
+        time_ptr->tm_sec = 0;
+        time_ptr->tm_min++;
+    }
+    if (time_ptr->tm_min > 60)
+    {
+        time_ptr->tm_min = 0;
+        time_ptr->tm_hour++;
+    }
+    if (time_ptr->tm_hour > 24)
+    {
+        time_ptr->tm_hour = 0;
+        time_ptr->tm_yday++;
+    }
+    int yday = time_ptr->tm_yday;
+    if (yday <= 31)
+        time_ptr->tm_mon = 0;
+    else if (yday -= 31 <= 28)
+        time_ptr->tm_mon = 1;
+    else if (yday -= 28 <= 31)
+        time_ptr->tm_mon = 2;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 3;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 4;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 5;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 6;
+    else if (yday -= 31 <= 31)
+        time_ptr->tm_mon = 7;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 8;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 9;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 10;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 11;
+    time_ptr->tm_mday = yday;
+};
+
+inline int convert_temp(char* dest, float temp, int units)
+{
+    char formatted[8];
+    int temp_i = (int)temp;
+    formatted[0] = '0' + temp_i / 100;
+    formatted[1] = '0' + temp_i / 10;
+    formatted[2] = '0' + temp_i % 10;
+    formatted[3] = '.';
+    temp_i = (int)(temp * 10);
+    formatted[4] = '0' + temp_i % 10;
+    formatted[5] = ' ';
+    if (units == 1)
+        formatted[6] = 'F';
+    else
+        formatted[6] = 'C';
+    formatted[7] = '\0';
+    memcpy(dest, formatted, 8);
+    return 0;
 };
 
 inline int convert_time(char* dest, struct tm * time_ptr)
@@ -16,9 +75,9 @@ inline int convert_time(char* dest, struct tm * time_ptr)
     formatted[4] = '0' + time_ptr->tm_min % 10;
     formatted[5] = ':';
     formatted[6] = '0' + time_ptr->tm_sec / 10;
-    formatted[7] = '0' + time_ptr->tm_sec & 10;
+    formatted[7] = '0' + time_ptr->tm_sec % 10;
     formatted[8] = '\0';
-    strcpy(dest, formatted);
+    memcpy(dest, formatted, 9);
     return 0;
 };
 
@@ -32,91 +91,125 @@ inline int convert_time(char* dest, struct tm * time_ptr)
  */
 inline int convert_date(char* dest, struct tm * time_ptr)
 {
-	char formatted[7];
-	switch(time_ptr->tm_mon)
-	{
-	case 0:
-		formatted[0] = 'J';
-		formatted[1] = 'a';
-		formatted[2] = 'n';
-		break;
-	case 1:
-		formatted[0] = 'F';
-		formatted[1] = 'e';
-		formatted[2] = 'b';
-		break;
-	case 2:
-		formatted[0] = 'M';
-		formatted[1] = 'a';
-		formatted[2] = 'r';
-		break;
-	case 3:
-		formatted[0] = 'A';
-		formatted[1] = 'p';
-		formatted[2] = 'r';
-		break;
-	case 4:
-		formatted[0] = 'M';
-		formatted[1] = 'a';
-		formatted[2] = 'y';
-		break;
-	case 5:
-		formatted[0] = 'J';
-		formatted[1] = 'u';
-		formatted[2] = 'n';
-		break;
-	case 6:
-		formatted[0] = 'J';
-		formatted[1] = 'u';
-		formatted[2] = 'l';
-		break;
-	case 7:
-		formatted[0] = 'A';
-		formatted[1] = 'u';
-		formatted[2] = 'g';
-		break;
-	case 8:
-		formatted[0] = 'S';
-		formatted[1] = 'e';
-		formatted[2] = 'p';
-		break;
-	case 9:
-		formatted[0] = 'O';
-		formatted[1] = 'c';
-		formatted[2] = 't';
-		break;
-	case 10:
-		formatted[0] = 'N';
-		formatted[1] = 'o';
-		formatted[2] = 'v';
-		break;
-	case 11:
-		formatted[0] = 'D';
-		formatted[1] = 'e';
-		formatted[2] = 'c';
-		break;
-	}
-	formatted[3] = ' ';
-	formatted[4] = '0' + time_ptr->tm_mday / 10;
-	formatted[5] = '0' + time_ptr->tm_mday % 10;
-	formatted[6] = '\0';
-	strcpy(dest, formatted);
+    char formatted[7];
+    switch (time_ptr->tm_mon)
+    {
+    case 0:
+        formatted[0] = 'J';
+        formatted[1] = 'a';
+        formatted[2] = 'n';
+        break;
+    case 1:
+        formatted[0] = 'F';
+        formatted[1] = 'e';
+        formatted[2] = 'b';
+        break;
+    case 2:
+        formatted[0] = 'M';
+        formatted[1] = 'a';
+        formatted[2] = 'r';
+        break;
+    case 3:
+        formatted[0] = 'A';
+        formatted[1] = 'p';
+        formatted[2] = 'r';
+        break;
+    case 4:
+        formatted[0] = 'M';
+        formatted[1] = 'a';
+        formatted[2] = 'y';
+        break;
+    case 5:
+        formatted[0] = 'J';
+        formatted[1] = 'u';
+        formatted[2] = 'n';
+        break;
+    case 6:
+        formatted[0] = 'J';
+        formatted[1] = 'u';
+        formatted[2] = 'l';
+        break;
+    case 7:
+        formatted[0] = 'A';
+        formatted[1] = 'u';
+        formatted[2] = 'g';
+        break;
+    case 8:
+        formatted[0] = 'S';
+        formatted[1] = 'e';
+        formatted[2] = 'p';
+        break;
+    case 9:
+        formatted[0] = 'O';
+        formatted[1] = 'c';
+        formatted[2] = 't';
+        break;
+    case 10:
+        formatted[0] = 'N';
+        formatted[1] = 'o';
+        formatted[2] = 'v';
+        break;
+    case 11:
+        formatted[0] = 'D';
+        formatted[1] = 'e';
+        formatted[2] = 'c';
+        break;
+    }
+    formatted[3] = ' ';
+    formatted[4] = '0' + time_ptr->tm_mday / 10;
+    formatted[5] = '0' + time_ptr->tm_mday % 10;
+    formatted[6] = '\0';
+    memcpy(dest, formatted, 7);
     return 0;
 };
 
 inline void increment_tm(struct tm * time_ptr, int seconds)
 {
-	time_ptr->tm_sec += seconds;
-	mktime(time_ptr);
+    time_ptr->tm_sec += seconds;
+    mktime(time_ptr);
 };
 
 inline void initialize_tm(struct tm * time_ptr)
 {
-	time_t rawtime = 8569460;
-	time_ptr = gmtime(&rawtime);
+    time_t rawtime = 8569460;
+    time_ptr->tm_yday = rawtime / 86400;
+    rawtime -= rawtime * 86400;
+    time_ptr->tm_hour = rawtime / 3600;
+    rawtime -= rawtime * 3600;
+    time_ptr->tm_min = rawtime / 60;
+    rawtime -= rawtime * 60;
+    time_ptr->tm_sec = rawtime;
+    time_ptr->tm_year = 0;
+    int yday = time_ptr->tm_yday;
+    if (yday <= 31)
+        time_ptr->tm_mon = 0;
+    else if (yday -= 31 <= 28)
+        time_ptr->tm_mon = 1;
+    else if (yday -= 28 <= 31)
+        time_ptr->tm_mon = 2;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 3;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 4;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 5;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 6;
+    else if (yday -= 31 <= 31)
+        time_ptr->tm_mon = 7;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 8;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 9;
+    else if (yday -= 31 <= 30)
+        time_ptr->tm_mon = 10;
+    else if (yday -= 30 <= 31)
+        time_ptr->tm_mon = 11;
+    time_ptr->tm_mday = yday;
+    mktime(time_ptr);
 };
 
 inline unsigned int utc_time_int(struct tm * time_ptr)
 {
-	return (unsigned int)mktime(time_ptr);
+    return (unsigned int)mktime(time_ptr);
 };
--- timeandtemp.c
+++ timeandtemp.c
@@ -9,10 +9,17 @@
 #include "peripherals.h"
 #include "stringutils.h"
 
-typedef enum {S_MENU, S_COUNTDOWN, S_PLAY, S_LOSE, S_WIN} state_t;
+typedef enum {S_RUN, S_EDIT} state_t;
+typedef enum {E_MON, E_DAY, E_HR, E_MIN, E_SEC} editstate_t;
 
 struct tm * global_time; //Global time counter.
 unsigned int timercount;
+
+#define CALADC12_15V_30C  *((unsigned int *)0x1A1A)
+#define CALADC12_15V_85C  *((unsigned int *)0x1A1C)
+
+unsigned int in_temp;
+char temp_changed;
 
 void swDelay(int numLoops);
 int read_push_button();
@@ -22,58 +29,139 @@ void set_touchpad(char states);
 void swDelayShort(int numLoops);
 void runtimerA2(void);
 void stoptimerA2(void);
+void convert_temps();
 
 #pragma vector=TIMER2_A0_VECTOR
 __interrupt void TimerA2_ISR(void)
 {
-	increment_tm(global_time, 1);
+    timercount++;
+    if (timercount == 60000)
+        timercount = 0;
+    if (timercount % 100 == 0)
+    {
+        increment_tm(global_time, 1);
+        convert_temps();
+    }
+}
+
+#pragma vector=ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+{
+    in_temp = ADC12MEM0;
+    temp_changed = 1;
 }
 
 void main(void)
 {
-	// Stop WDT
-    WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
+    volatile float temperatureDegC;
+    volatile float temperatureDegF;
+    volatile float degC_per_bit;
+    volatile unsigned int bits30, bits85;
+    // Stop WDT
+    WDTCTL = WDTPW | WDTHOLD;       // Stop watchdog timer
 
+    REFCTL0 &= ~REFMSTR;    // Reset REFMSTR to hand over control of
+    // internal reference voltages to
+    // ADC12_A control registers
+
+    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON;     // Internal ref = 1.5V
+    ADC12CTL1 = ADC12SHP;                     // Enable sample timer
+    // Using ADC12MEM0 to store reading
+    ADC12MCTL0 = ADC12SREF_1 + ADC12INCH_10;  // ADC i/p ch A10 = temp sense
+    // ACD12SREF_1 = internal ref = 1.5v
+
+    __delay_cycles(100);                    // delay to allow Ref to settle
+    ADC12CTL0 |= ADC12ENC;                // Enable conversion
+    // Use calibration data stored in info memory
+    bits30 = CALADC12_15V_30C;
+    bits85 = CALADC12_15V_85C;
+    degC_per_bit = ((float)(85.0 - 30.0)) / ((float)(bits85 - bits30));
+
+    ADC12IE = BIT0;
     _BIS_SR(GIE);
 
     //Perform initializations (see peripherals.c)
     configTouchPadLEDs();
     configDisplay();
     configCapButtons();
+    global_time = (struct tm*)malloc(sizeof(struct tm));
     initialize_tm(global_time);
     GrClearDisplay(&g_sContext);
-    while(1)
+
+    state_t state = S_RUN;
+    editstate_t editstate = E_MON;
+
+    char* time_str = (char*)malloc(sizeof(char) * 9);
+    char* date_str = (char*)malloc(sizeof(char) * 7);
+    char* temp_c_str = (char*)malloc(sizeof(char) * 8);
+    char* temp_f_str = (char*)malloc(sizeof(char) * 8);
+    convert_time(time_str, global_time);
+    convert_date(date_str, global_time);
+    runtimerA2();
+    while (1)
     {
+        switch (state)
+        {
+        case S_RUN:
+            convert_time(time_str, global_time);
+            convert_date(date_str, global_time);
+            break;
+        case S_EDIT:
+            break;
+        }
+
+        if (temp_changed)
+        {
+            temperatureDegC = (float)((long)in_temp - CALADC12_15V_30C) * degC_per_bit + 30.0;
+            temperatureDegF = temperatureDegC * 9.0 / 5.0 + 32.0;
+            temp_changed = 0;
+        }
+        GrClearDisplay(&g_sContext);
+        convert_temp(temp_c_str, temperatureDegC, 0);
+        convert_temp(temp_f_str, temperatureDegF, 1);
+        GrStringDrawCentered(&g_sContext, time_str, AUTO_STRING_LENGTH, 51, 10, TRANSPARENT_TEXT);
+        GrStringDrawCentered(&g_sContext, date_str, AUTO_STRING_LENGTH, 51, 20, TRANSPARENT_TEXT);
+        GrStringDrawCentered(&g_sContext, temp_c_str, AUTO_STRING_LENGTH, 51, 30, TRANSPARENT_TEXT);
+        GrStringDrawCentered(&g_sContext, temp_f_str, AUTO_STRING_LENGTH, 51, 40, TRANSPARENT_TEXT);
+        GrFlush(&g_sContext);
+        __no_operation();                       // SET BREAKPOINT HERE
     }
+}
+
+void convert_temps()
+{
+    ADC12CTL0 &= ~ADC12SC;  // clear the start bit
+    ADC12CTL0 |= ADC12SC;       // Sampling and conversion start
+    // Single conversion (single channel)
 }
 
 void runtimerA2(void)
 {
-	TA2CTL = TASSEL_1 + MC_1 + ID_0;
-	TA2CCR0 = 163;
-	TA2CCTL0 = CCIE;
+    TA2CTL = TASSEL_1 + MC_1 + ID_0;
+    TA2CCR0 = 327;
+    TA2CCTL0 = CCIE;
 }
 
 void stoptimerA2(void)
 {
-	TA2CTL = MC_0;
-	TA2CCTL0 &= ~CCIE;
+    TA2CTL = MC_0;
+    TA2CCTL0 &= ~CCIE;
 }
 
 int read_push_button()
 {
-	P1SEL &= ~BIT7;
-	P2SEL &= ~BIT2;
-	P1DIR &= ~BIT7;
-	P2DIR &= ~BIT2;
-	P1REN |= BIT7;
-	P2REN |= BIT2;
-	P1OUT &= BIT7;
-	P2OUT &= BIT2;
-	if ((P1IN & BIT7) == 0 && (P2IN & BIT2) == 0) return 3;
-	else if ((P1IN & BIT7) == 0) return 1;
-	else if ((P2IN & BIT2) == 0) return 2;
-	else return 0;
+    P1SEL &= ~BIT7;
+    P2SEL &= ~BIT2;
+    P1DIR &= ~BIT7;
+    P2DIR &= ~BIT2;
+    P1REN |= BIT7;
+    P2REN |= BIT2;
+    P1OUT &= BIT7;
+    P2OUT &= BIT2;
+    if ((P1IN & BIT7) == 0 && (P2IN & BIT2) == 0) return 3;
+    else if ((P1IN & BIT7) == 0) return 1;
+    else if ((P2IN & BIT2) == 0) return 2;
+    else return 0;
 }
 
 void configLED1_3(char inbits)
@@ -91,30 +179,30 @@ void configLED1_3(char inbits)
 
 void set_touchpad(char states)
 {
-	P1OUT &= ~(TOUCHPAD_1|TOUCHPAD_2|TOUCHPAD_3|TOUCHPAD_4|TOUCHPAD_5);
-	P1OUT |= (states);
+    P1OUT &= ~(TOUCHPAD_1 | TOUCHPAD_2 | TOUCHPAD_3 | TOUCHPAD_4 | TOUCHPAD_5);
+    P1OUT |= (states);
 }
 
 int get_touchpad()
 {
     //Get touchpad button pressed. Returns column that the button corresponds to.
-	switch (CapButtonRead())
-	{
-	case BUTTON_NONE:
-		return -1;
-	case BUTTON_X:
-		return TOUCHPAD_1;
-	case BUTTON_SQ:
-		return TOUCHPAD_2;
-	case BUTTON_OCT:
-		return TOUCHPAD_3;
-	case BUTTON_TRI:
-		return TOUCHPAD_4;
-	case BUTTON_CIR:
-		return TOUCHPAD_5;
-	default:
-		return -1;
-	}
+    switch (CapButtonRead())
+    {
+    case BUTTON_NONE:
+        return -1;
+    case BUTTON_X:
+        return TOUCHPAD_1;
+    case BUTTON_SQ:
+        return TOUCHPAD_2;
+    case BUTTON_OCT:
+        return TOUCHPAD_3;
+    case BUTTON_TRI:
+        return TOUCHPAD_4;
+    case BUTTON_CIR:
+        return TOUCHPAD_5;
+    default:
+        return -1;
+    }
 }
 
 /**
